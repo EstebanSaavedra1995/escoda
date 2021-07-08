01357b4730feabf986adf728cf73e4344cd8ce63
Esteban Saavedra <saavedra.esteban.1995@gmail.com>
2021-07-07 21:05:56 -0300

    mas cambios

--- public/js/construccion.js
+++ public/js/construccion.js
@@ -7,10 +7,39 @@ document.getElementById('piezas').addEventListener('change', function (e) {
         method: 'POST',
         body: datos,
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-       
+
+        .then(res => res.json())
+        .then(data => {
+            if (data.length == 0) {
+                limpiarDatos();
+            } else {
+                console.log(data);
+                let material = document.getElementById('material');
+                material.value = `${data.material.CodigoMaterial} - ${data.material.Material} - ${data.material.Dimension} - ${data.material.Calidad}`;
+                let longcorte = document.getElementById('longcorte');
+                longcorte.value = data.materialPieza.longitudCorte;
+                let contenidotabla = document.getElementById('contenidotabla');
+                let tabla='';
+                data.coladaMaterial.map(colada => {
+                    tabla += `<tr>`;
+                    tabla += `<td> ${colada.Colada} </td>`;
+                    tabla += `<td> ${colada.Stock} </td>`;
+                    tabla += `</tr>`;
+                })
+                contenidotabla.innerHTML = tabla;
+            }
         })
 }, true)
 
+const limpiarDatos = () => {
+    let material = document.getElementById('material');
+    material.value = '';
+    let longcorte = document.getElementById('longcorte');
+    longcorte.value = ''
+    let contenidotabla = document.getElementById('contenidotabla');
+    contenidotabla.innerHTML = '';
+}
+
+$(document).ready(function(){
+    $('#')
+})
