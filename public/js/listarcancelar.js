5bb0195b8c0142c2655180fae849bed7967cb777
Esteban Saavedra <saavedra.esteban.1995@gmail.com>
2021-08-05 21:14:03 -0300

    orden

---
+++ public/js/listarcancelar.js
@@ -0,0 +1,301 @@
+document.getElementById('lista').addEventListener('change', function (e) {
+    let buscar = document.getElementById('buscar');
+    buscar.disabled = false;
+    let valor = document.getElementById('lista').value;
+    if (valor == 0) {
+        filtroNroOrden();
+    } else if (valor == 1) {
+        filtroFecha();
+    } else {
+        filtroPieza();
+    }
+
+})
+
+const filtroNroOrden = () => {
+    let filtro = document.getElementById('filtro');
+    filtro.className = "row mb-2";
+    let input = `<label class=" col mr-2">Nro orden de construcción:</label>`;
+    input += `<input type="number" name= "nroorden" id="nroorden" class="form-control col mr-2">`
+    filtro.innerHTML = input;
+
+}
+
+const filtroFecha = () => {
+    let hoy = formatDate();
+
+    let filtro = document.getElementById('filtro');
+    filtro.className = "row mb-2";
+    let inputs = `<label class=" col mr-2">Desde</label>`;
+    inputs += `<input type="date" value="${hoy}" name="fecha1" id="fecha1" class="form-control col mr-2">`
+    inputs += `<label class=" col mr-2">Hasta</label>`;
+    inputs += `<input type="date" value="${hoy}" name="fecha2" id="fecha2" class="form-control col mr-2">`
+    filtro.innerHTML = inputs;
+
+}
+const filtroPieza = () => {
+    let filtro = document.getElementById('filtro');
+    filtro.className = "row mb-2";
+
+    const datos = new FormData(document.getElementById('formulario'));
+    fetch('/admin/listarcancelar/piezas', {
+        method: 'POST',
+        body: datos,
+    })
+        .then(res => res.json())
+        .then(data => {
+            let comboPieza = document.createElement("select");
+            comboPieza.className = "col mr-2";
+            comboPieza.name = "pieza";
+            comboPieza.id = "pieza";
+
+            data.forEach(piezas => {
+                let option = document.createElement('option');
+                option.innerHTML = `${piezas.CodPieza} - ${piezas.NombrePieza} - ${piezas.Medida}`;
+                option.value = piezas.CodPieza;
+                comboPieza.appendChild(option);
+            })
+            filtro.innerHTML = `<label class="col mr-2">Pieza:</label>`;
+            filtro.appendChild(comboPieza);
+        })
+}
+
+const listarOrdenes = () => {
+    const datos = new FormData(document.getElementById('formulario'));
+    fetch('/admin/listarcancelar/ordenes', {
+        method: 'POST',
+        body: datos,
+    })
+        .then(res => res.json())
+        .then(data => {
+            divtablatareas = document.getElementById('divtablatareas');
+            divtablatareas.innerHTML = '';
+            if (data[0] === 'ordenes') {
+                realizarTablaOrden(data[1]);
+            } else if (data[0] === 'piezas')
+                realizarTablaPieza(data[1]);
+            else {
+                realizarTablaFecha(data[1]);
+            }
+        })
+}
+const formatDate = () => {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+const formatoFecha = (fecha) => {
+
+    day = fecha[4] + fecha[5];
+    month = fecha[2] + fecha[3];
+    year = fecha[0] + fecha[1];
+    return [day, month, year].join('/');
+}
+
+const listarTareas = (oc) => {
+    const datos = new FormData(document.getElementById('formulario'));
+    datos.append('oc', oc);
+    fetch('/admin/listarcancelar/detalles', {
+        method: 'POST',
+        body: datos,
+    })
+        .then(res => res.json())
+        .then(data => {
+            divtablatareas = document.getElementById('divtablatareas');
+            let tabla = `<table class="table-striped table table-bordered table-scroll3">`;
+            tabla += `<thead>`;
+            tabla += `<tr>`;
+            tabla += `<th scope="col">Tareas</th>`;
+            tabla += `<th scope="col">Máquina</th>`;
+            tabla += `<th scope="col">Operario</th>`;
+            tabla += `<th scope="col">Supervisor</th>`;
+            tabla += `<th scope="col">Horas</th>`;
+            tabla += `</tr>`;
+            tabla += `</thead>`;
+            tabla += `<tbody>`;
+            //console.log(data);
+            data.forEach(tarea => {
+                tabla += `<tr>`;
+                tabla += `<td>${tarea.Tarea}</td>`;
+                tabla += `<td>${tarea.Maquina}</td>`;
+                tabla += `<td>${tarea.Operario}</td>`;
+                tabla += `<td>${tarea.Supervisor}</td>`;
+                tabla += `<td>${tarea.Horas}</td>`;
+                tabla += `</tr>`;
+            })
+            tabla += `</tbody>`;
+            tabla += `</table>`;
+            divtablatareas.innerHTML = tabla;
+        })
+}
+
+const cancelarTarea = (oc) => {
+    swal({
+        title: `¿Desea cancelar la orden N°${oc}?`,
+        icon: "warning",
+        buttons: ["Cancelar", "Aceptar"],
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                const datos = new FormData(document.getElementById('formulario'));
+                datos.append('numoc', oc);
+                fetch('/admin/listarcancelar/cancelar', {
+                    method: 'POST',
+                    body: datos,
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data === 'ok') {
+                            swal({
+                                title: `¡Se ha dado de baja la orden de construcción N°${oc}!`,
+                                icon: "success",
+                                button: "Aceptar",
+                            });
+                            setTimeout(function () {
+                                window.location.href = "http://escoda.test/admin/listarcancelar";
+                            }, 1500)
+                        } else {
+                            swal({
+                                title: "¡Ocurrió un fallo, no se pudo dar de baja!",
+                                icon: "warning",
+                                button: "Aceptar",
+                            });
+                        }
+                    })
+            }
+        });
+}
+
+const excel = () => {
+    /*   const datos = new FormData(document.getElementById('formulario'));
+      fetch('/admin/listarcancelar/exportExcel', {
+          method: 'GET',
+      })
+          
+          .then(data => {
+              console.log('LLegó');
+          }) */
+    console.log('Excel');
+}
+
+const realizarTablaPieza = (array) => {
+    let divtabla = document.getElementById('divtabla');
+    let tabla = `<table class="table-striped table table-bordered table-scroll3">`;
+    tabla += `<thead>`;
+    tabla += `<tr>`;
+    tabla += `<th scope="col">Pieza</th>`;
+    tabla += `<th scope="col">Nro de orden</th>`;
+    tabla += `<th scope="col">Fecha</th>`;
+    tabla += `<th scope="col">Cantidad</th>`;
+    tabla += `<th scope="col">Material</th>`;
+    tabla += `<th scope="col">Longitud corte</th>`;
+    tabla += `<th scope="col">Colada</th>`;
+    tabla += `<th scope="col">Acciónes</th>`;
+    tabla += `</tr>`;
+    tabla += `</thead>`;
+    tabla += `<tbody>`;
+    array.forEach(orden => {
+        tabla += `<tr>`;
+        tabla += `<td>${orden.CodPieza} - ${orden.NombrePieza} - ${orden.Medida}</td>`;
+        tabla += `<td style="width: 20px">${orden.NroOC} </td>`;
+        tabla += `<td style="width: 15px">${formatoFecha(orden.Fecha)} </td>`;
+        tabla += `<td style="width: 15px">${orden.Cantidad} </td>`;
+        tabla += `<td>${orden.CodigoMaterial} - ${orden.Material} - ${orden.Dimension} - ${orden.Calidad}</td>`;
+        tabla += `<td style="width: 20px">${orden.LongitudCorte} </td>`;
+        tabla += `<td style="width: 15px">${orden.Colada} </td>`;
+        tabla += `<td><button type="button" class="btn btn-info" onclick="listarTareas('${orden.NroOC}');">Tareas</button>`;
+        tabla += `<button type="button" class="btn btn-secondary">Imprimir</button>`
+        tabla += `<button type="button" class="btn btn-danger" onclick="cancelarTarea('${orden.NroOC}');">Cancelar</button></td>`;
+        tabla += `</tr>`;
+
+    });
+
+    tabla += `</tbody>`;
+    tabla += `</table>`;
+    tabla += `<tr> <td> <button type ="button" class="btn btn-success" onclick="excel();">Excel</button></td></tr>`
+    divtabla.innerHTML = tabla;
+
+}
+const realizarTablaOrden = (array) => {
+    let divtabla = document.getElementById('divtabla');
+    let tabla = `<table class="table-striped table table-bordered table-scroll3">`;
+    tabla += `<thead>`;
+    tabla += `<tr>`;
+    tabla += `<th scope="col">Nro de orden</th>`;
+    tabla += `<th scope="col">Fecha</th>`;
+    tabla += `<th scope="col">Pieza</th>`;
+    tabla += `<th scope="col">Cantidad</th>`;
+    tabla += `<th scope="col">Material</th>`;
+    tabla += `<th scope="col">Longitud corte</th>`;
+    tabla += `<th scope="col">Colada</th>`;
+    tabla += `<th scope="col">Acciónes</th>`;
+    tabla += `</tr>`;
+    tabla += `</thead>`;
+    tabla += `<tbody>`;
+    array.forEach(orden => {
+        tabla += `<tr>`;
+        tabla += `<td style="width: 20px">${orden.NroOC} </td>`;
+        tabla += `<td style="width: 15px">${formatoFecha(orden.Fecha)} </td>`;
+        tabla += `<td>${orden.CodPieza} - ${orden.NombrePieza} - ${orden.Medida}</td>`;
+        tabla += `<td style="width: 15px">${orden.Cantidad} </td>`;
+        tabla += `<td>${orden.CodigoMaterial} - ${orden.Material} - ${orden.Dimension} - ${orden.Calidad}</td>`;
+        tabla += `<td style="width: 20px">${orden.LongitudCorte} </td>`;
+        tabla += `<td style="width: 15px">${orden.Colada} </td>`;
+        tabla += `<td><button type="button" class="btn btn-info" onclick="listarTareas('${orden.NroOC}');">Tareas</button>`;
+        tabla += `<button type="button" class="btn btn-secondary">Imprimir</button>`
+        tabla += `<button type="button" class="btn btn-danger" onclick="cancelarTarea('${orden.NroOC}');">Cancelar</button></td>`;
+        tabla += `</tr>`;
+
+    });
+
+    tabla += `</tbody>`;
+    tabla += `</table>`;
+    tabla += `<tr> <td> <button type ="button" class="btn btn-success" onclick="excel();">Excel</button></td></tr>`
+    divtabla.innerHTML = tabla;
+
+}
+const realizarTablaFecha = (array) => {
+    let divtabla = document.getElementById('divtabla');
+    let tabla = `<table class="table-striped table table-bordered table-scroll3">`;
+    tabla += `<thead>`;
+    tabla += `<tr>`;
+    tabla += `<th scope="col">Fecha</th>`; style = "width: 20px"
+    tabla += `<th scope="col">Nro de orden</th>`;
+
+    tabla += `<th scope="col">Pieza</th>`;
+    tabla += `<th scope="col">Cantidad</th>`;
+    tabla += `<th scope="col">Material</th>`;
+    tabla += `<th scope="col">Longitud corte</th>`;
+    tabla += `<th scope="col">Colada</th>`;
+    tabla += `<th scope="col">Acciónes</th>`;
+    tabla += `</tr>`;
+    tabla += `</thead>`;
+    tabla += `<tbody>`;
+    array.forEach(orden => {
+        tabla += `<tr>`;
+        tabla += `<td style="width: 15px">${formatoFecha(orden.Fecha)} </td>`;
+        tabla += `<td style="width: 20px">${orden.NroOC} </td>`;
+        tabla += `<td>${orden.CodPieza} - ${orden.NombrePieza} - ${orden.Medida}</td>`;
+        tabla += `<td style="width: 15px">${orden.Cantidad} </td>`;
+        tabla += `<td>${orden.CodigoMaterial} - ${orden.Material} - ${orden.Dimension} - ${orden.Calidad}</td>`;
+        tabla += `<td style="width: 20px">${orden.LongitudCorte} </td>`;
+        tabla += `<td style="width: 15px">${orden.Colada} </td>`;
+        tabla += `<td><button type="button" class="btn btn-info" onclick="listarTareas('${orden.NroOC}');">Tareas</button>`;
+        tabla += `<button type="button" class="btn btn-secondary">Imprimir</button>`
+        tabla += `<button type="button" class="btn btn-danger" onclick="cancelarTarea('${orden.NroOC}');">Cancelar</button></td>`;
+        tabla += `</tr>`;
+
+    });
+    tabla += `</tbody>`;
+    tabla += `</table>`;
+    tabla += `<tr> <td> <button type ="button" class="btn btn-success" onclick="excel();">Excel</button></td></tr>`
+    divtabla.innerHTML = tabla;
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\HorariosMaquinasController;
 use App\Http\Controllers\Admin\Ordenes\ConstruccionController;
 use App\Http\Controllers\Admin\RegistrarEgresosController;
 use App\Http\Controllers\Admin\Stock\ConfeccionarDespieceController;
+use App\Http\Controllers\Admin\Ordenes\ListarCancelarController;
+use App\Http\Controllers\Admin\Ordenes\ReparacionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,20 +18,31 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/admin', function () {
     return view('layouts.index');
 })->name('index');
 
-Route::get('/admin/construccion',[ConstruccionController::class,'index'])->name('construccion.confeccionar');
-Route::post('/admin/construccion',[ConstruccionController::class,'piezas']);
-Route::post('/admin/construccion/material',[ConstruccionController::class,'material']);
-Route::post('/admin/construccion/material/buscar',[ConstruccionController::class,'buscarMaterial']);
-Route::post('/admin/construccion/modificartarea',[ConstruccionController::class,'modificarTarea']);
-Route::post('/admin/construccion/agregarconstruccion',[ConstruccionController::class,'agregarconstruccion']);
+Route::get('/admin/construccion', [ConstruccionController::class, 'index'])->name('construccion.confeccionar');
+Route::post('/admin/construccion', [ConstruccionController::class, 'piezas']);
+Route::post('/admin/construccion/material', [ConstruccionController::class, 'material']);
+Route::post('/admin/construccion/material/buscar', [ConstruccionController::class, 'buscarMaterial']);
+Route::post('/admin/construccion/modificartarea', [ConstruccionController::class, 'modificarTarea']);
+Route::post('/admin/construccion/agregarconstruccion', [ConstruccionController::class, 'agregarconstruccion']);
 
-Route::get('/admin/horariosmaquinas',[HorariosMaquinasController::class,'index'])->name('horarios.maquinas');
-Route::get('/admin/confeccionardespiece',[ConfeccionarDespieceController::class,'index'])->name('confeccionar.despiece');
-Route::post('/admin/confeccionardespiecepiezas',[ConfeccionarDespieceController::class,'piezas']);
-Route::post('/admin/confeccionardespiecetabla',[ConfeccionarDespieceController::class,'tabla']);
-Route::post('/admin/confeccionardespiecepredeterminar',[ConfeccionarDespieceController::class,'predeterminar']);
-Route::get('/admin/registraregreso',[RegistrarEgresosController::class,'index'])->name('registrar.egresos');
-Route::post('/admin/registraregresopiezas',[RegistrarEgresosController::class,'piezas']);
-Route::post('/admin/registraregresotabla',[RegistrarEgresosController::class,'tabla']);
-Route::get('/admin/controlhorariosmaquina',[ControlHorariosMaquinaController::class,'index'])->name('control.horarios.maquina');
+Route::get('/admin/listarcancelar', [ListarCancelarController::class, 'index'])->name('construccion.listarcancelar');
+Route::post('/admin/listarcancelar/piezas', [ListarCancelarController::class, 'piezas']);
+Route::post('/admin/listarcancelar/ordenes', [ListarCancelarController::class, 'ordenes']);
+Route::post('/admin/listarcancelar/detalles', [ListarCancelarController::class, 'detalles']);
+Route::post('/admin/listarcancelar/cancelar', [ListarCancelarController::class, 'cancelar']);
+Route::post('/admin/listarcancelar/excel', [ListarCancelarController::class, 'excel']);
+Route::get('/admin/listarcancelar/exportExcel', [ListarCancelarController::class, 'exportExcel']);
 
+Route::get('/admin/reparacion/confeccionar', [ReparacionController::class, 'index'])->name('reparacion.confeccionar');
+
+
+
+Route::get('/admin/horariosmaquinas', [HorariosMaquinasController::class, 'index'])->name('horarios.maquinas');
+Route::get('/admin/confeccionardespiece', [ConfeccionarDespieceController::class, 'index'])->name('confeccionar.despiece');
+Route::post('/admin/confeccionardespiecepiezas', [ConfeccionarDespieceController::class, 'piezas']);
+Route::post('/admin/confeccionardespiecetabla', [ConfeccionarDespieceController::class, 'tabla']);
+Route::post('/admin/confeccionardespiecepredeterminar', [ConfeccionarDespieceController::class, 'predeterminar']);
+Route::get('/admin/registraregreso', [RegistrarEgresosController::class, 'index'])->name('registrar.egresos');
+Route::post('/admin/registraregresopiezas', [RegistrarEgresosController::class, 'piezas']);
+Route::post('/admin/registraregresotabla', [RegistrarEgresosController::class, 'tabla']);
+Route::get('/admin/controlhorariosmaquina', [ControlHorariosMaquinaController::class, 'index'])->name('control.horarios.maquina');
